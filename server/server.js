e5d17048a9c74ba6c511c939a15c2d0c2695c2c1
seanml <i.r.sean@gmail.com>
2017-01-13 13:26:40 -0800

    [Feature] Basic server running

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 7777;
+
+app.use(bodyParser.json());
+
+app.use(express.static('./client'));
+
+app.use(cors());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('client', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is listening on port: ', port);
+});
